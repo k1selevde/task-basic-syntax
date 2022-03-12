3faf38f869fb7d1ecb67df9e1a5abb0f9a067fe6
Denis <k1selevde@yandex.ru>
2022-03-12 15:42:51 +0500

    task basic syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,24 @@
+const dictMap = new Map([
+    ['I', 1],
+    ['V', 5],
+    ['X', 10],
+    ['L', 50],
+    ['C', 100],
+    ['D', 500],
+    ['M', 1000],
+])
+
 export function romanToInteger(str) {
-    let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    let result = 0, previous = 0;
+
+    const revSymbols = str.split('').reverse()
+
+    for (const symbol of revSymbols) {
+        let current = dictMap.get(symbol);
+
+        current >= previous ? result += current : result -= current
+
+        previous = current;
+    }
     return result;
 }
